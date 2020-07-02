6cbb69ab29ad7d16d447f0eef7e23c4620dedaeb
Jennie Dalgren <jennie.dalgren@gmail.com>
2020-07-02 13:07:24 +0200

    add google analytics plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,24 +7,23 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
 
 import { Header } from "./header"
 import { Footer } from "./footer"
 import "./layout.css"
 
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+`
+
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-      </div>
+      <Wrapper>{children}</Wrapper>
       <Footer />
     </>
   )
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,10 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import astro from "../images/gatsby-astronaut.png"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
+    <img src={astro} alt="astronauten" />
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
